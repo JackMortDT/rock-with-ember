b5dd7636b76f7cdfc5bd46c0dc19f7ada8db7755
Luis Sastre <luis@makingdevs.com>
2018-03-25 20:35:04 -0600

    Adding bootstrap to the project

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,27 +1,34 @@
 import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
 
+var Song = EmberObject.extend({
+  title: '',
+  band: '',
+  rating: 0
+});
+
 export default Route.extend({
+	
 	model(){
-		var theWineSong = EmberObject.create({
+		var theWineSong = Song.create({
 			title: 'The Wine Song',
 			band: 'The cat empire',
 			rating: 5
 		});
 
-		var stillYoung = EmberObject.create({
+		var stillYoung = Song.create({
 			title: 'Still Young',
 			band: 'The cat empire',
 			rating: 4
 		});
 
-		var mardyBum = EmberObject.create({
+		var mardyBum = Song.create({
 			title: 'Mardy Bum',
 			band: 'Artic Monkeys',
 			rating: 4
 		});
 
-		var mercy = EmberObject.create({
+		var mercy = Song.create({
 			title: 'Mercy',
 			band: 'Muse',
 			rating: 3
@@ -29,4 +36,6 @@ export default Route.extend({
 
 		return [theWineSong, stillYoung, mardyBum, mercy]
 	}
+
+
 });
