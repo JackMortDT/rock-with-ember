f8132337e7b24a6c0fd40c1a05f700a6c365db6b
Luis Sastre <luis@makingdevs.com>
2018-03-26 20:08:43 -0600

    Adding model and template to bands

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+var Band = EmberObject.extend({
+	name: '',
+});
 
 export default Route.extend({
+
+	model(){
+		var theCatEmpire = Band.create({ name: 'The Cat Empire' });
+		var arcticMonkeys = Band.create({ name: 'Artic Monkeys'});
+		var muse = Band.create({ name: 'Muse'});
+
+		return [theCatEmpire, arcticMonkeys, muse];
+	}
+
 });
